3d05e4a0659d34102ebd476d8fff4790309bdda6
Kyle Bebak <kylebebak@gmail.com>
2018-02-07 09:13:52 -0600

    All backend containers use the same image with a different command, this makes deployment MUCH easier, and works well in both dev and production

--- src/api.dev.sh
+++ src/api.dev.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 . ./.env
+
+until psql postgres://postgres:password@db/postgres -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - continuing"
+
+python manage.py migrate --noinput
+python manage.py superuser --email $CUSTOM_SUPERUSER_EMAIL --password $CUSTOM_SUPERUSER_PASSWORD
+
 python manage.py runserver 0.0.0.0:8000
