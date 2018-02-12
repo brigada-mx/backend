ae26fdb63058314ad12e600b255f792d998b26b2
Kyle Bebak <kylebebak@gmail.com>
2018-02-11 18:35:58 -0600

    Serve django static files, including admin site static files, with uwsgi

--- src/api.prod.sh
+++ src/api.prod.sh
@@ -8,8 +8,9 @@ done
 
 >&2 echo "Postgres is up - continuing"
 
-python manage.py collecstatic
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
 python manage.py superuser --email $CUSTOM_SUPERUSER_EMAIL --password $CUSTOM_SUPERUSER_PASSWORD
 
 uwsgi --http-auto-chunked --http-keepalive --ini uwsgi.ini
--- src/db/users/models.py
+++ src/db/users/models.py
@@ -1,13 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 from db.config import BaseModel
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, **kwargs):
+        """
+        Creates and saves a generic user with the given email, first_name, surname
+        and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=kwargs.pop('first_name', ''),
+            surname=kwargs.pop('surname', ''),
+            **kwargs
+        )
+
+        password = kwargs.pop('password', None)
+        if not password:
+            raise ValueError('Users must have a password')
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, *args, **kwargs):
+        user = self.create_user(*args, **kwargs)
+        user.is_active = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class CustomAbstractBaseUser(AbstractBaseUser, BaseModel, PermissionsMixin):
     email = models.EmailField(unique=True, db_index=True)
     full_name = models.CharField(max_length=100, db_index=True)
     is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name']
