9b2bdd5eb90fafe82534981376808932743b7cff
Kyle Bebak <kylebebak@gmail.com>
2018-02-16 12:52:12 -0600

    Adds API URLs to urls.py, no views yet

--- src/api/urls.py
+++ src/api/urls.py
@@ -29,6 +29,16 @@ urlpatterns = [
     url(r'^organizations/(?P<pk>[0-9]+)/$', views.OrganizationDetail.as_view(), name='organization-detail'),
 
     url(r'^establishments/$', cache_page(60 * 30)(views.EstablishmentList.as_view()), name='establishment-list'),
+
+    url(r'^account/organization/$', views.AccountOrganizationDetail.as_view(), name='account-organization-detail'),
+    url(r'^account/actions/$', views.AccountActionCreate.as_view(), name='account-action-create'),
+    url(r'^account/actions/(?P<pk>[0-9]+)$', views.AccountActionDetail.as_view(), name='account-action-detail'),
+    url(r'^account/submissions/(?P<pk>[0-9]+)/$',
+        views.AccountSubmissionDetail.as_view(), name='account-submission-detail'),
+    url(r'^account/reset_key/$', views.AccountResetKey.as_view(), name='account-reset-key'),
+    url(r'^account/token/$', views.AccountToken.as_view(), name='account-token'),
+    url(r'^account/set_password/$', views.AccountSetPassword.as_view(), name='account-set-password'),
+    url(r'^account/send_set_password_email/$', views.SendSetPasswordEmail.as_view(), name='send-set-password-email'),
 ]
 
 # allow API to parse and return many different formats, not just default JSON
