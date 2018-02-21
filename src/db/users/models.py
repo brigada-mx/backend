da835ae16874741656e7eed067575cf3e0c85a68
Kyle Bebak <kylebebak@gmail.com>
2018-02-21 12:18:12 -0600

    Slightly refactors send_set_password_email to conform with principle of least surprise

--- src/db/users/models.py
+++ src/db/users/models.py
@@ -92,23 +92,33 @@ class OrganizationUser(CustomAbstractBaseUser):
         if self.pk is None:
             if not self.password:
                 self.set_unusable_password()
-            self.send_set_password_email()
+            self.send_set_password_email(save=False, reset=False)
         return super().save(*args, **kwargs)
 
     @property
     def is_organization_user(self):
         return True
 
-    def send_set_password_email(self):
+    def send_set_password_email(self, save=True, reset=True):
         self.set_password_token_created = timezone.now()
         self.set_password_token = binascii.hexlify(os.urandom(30)).decode()
+        if save:
+            self.save()
 
-        body = """¡Gracias por crear tu cuenta con Sintonía!<br><br>
-        Para activarla y usar la plataforma, dale clic en la liga y define tu contraseña.<br><br>
-        <a href="https://app.ensintonia.org/set_password?token={}" target="_blank">Activar Tu Cuenta</a>
-        """.format(self.set_password_token)
+        if reset:
+            subject = 'Restablecer tu contraseña Sintonía'
+            body = """Dale clic en la liga para restablecer tu contraseña.<br><br>
+            Si no pediste restablecer tu contraseña puedes borrar este email.<br><br>
+            <a href="https://app.ensintonia.org/set_password?token={}" target="_blank">Restablecer Tu Contraseña</a>
+            """.format(self.set_password_token)
+        else:
+            subject = 'Activa tu cuenta Sintonía'
+            body = """¡Gracias por crear tu cuenta con Sintonía!<br><br>
+            Para activarla y usar la plataforma, dale clic en la liga y define tu contraseña.<br><br>
+            <a href="https://app.ensintonia.org/set_password?token={}" target="_blank">Activar Tu Cuenta</a>
+            """.format(self.set_password_token)
 
-        send_email.delay([self.email], 'Activa tu cuenta Sintonía', body)
+        send_email.delay([self.email], subject, body)
 
 
 class TokenBaseModel(models.Model):
